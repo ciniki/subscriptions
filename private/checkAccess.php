9bfc40be20da0e746972fec9942ded9504857d53
Andrew Rivett <veggiefrog@gmail.com>
2012-05-26 23:03:00 +0000

    Removed features module, cleaned up checkAccess and added business checkModuleAccess function

--- private/checkAccess.php
+++ private/checkAccess.php
@@ -15,31 +15,24 @@
 //
 function ciniki_subscriptions_checkAccess($ciniki, $business_id, $method, $subscription_id) {
 	//
+	// Check if the business is active and the module is enabled
+	//
+	require_once($ciniki['config']['core']['modules_dir'] . '/businesses/private/checkModuleAccess.php');
+	$rc = ciniki_businesses_checkModuleAccess($ciniki, $business_id, 'ciniki', 'subscriptions');
+	if( $rc['stat'] != 'ok' ) {
+		return $rc;
+	}
+
+	if( !isset($rc['ruleset']) ) {
+		return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'382', 'msg'=>'No permissions granted'));
+	}
+	$modules = $rc['modules'];
+
+	//
 	// Load the rulesets for this module
 	//
 	require_once($ciniki['config']['core']['modules_dir'] . '/subscriptions/private/getRulesets.php');
 	$rulesets = ciniki_subscriptions_getRuleSets($ciniki);
-
-	//
-	// Check if the module is turned on for the business
-	// Check the business is active
-	// Get the ruleset for this module
-	//
-	$strsql = "SELECT ruleset FROM ciniki_businesses, ciniki_business_modules "
-		. "WHERE ciniki_businesses.id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
-		. "AND ciniki_businesses.status = 1 "														// Business is active
-		. "AND ciniki_businesses.id = ciniki_business_modules.business_id "
-		. "AND ciniki_business_modules.package = 'ciniki' "
-		. "AND ciniki_business_modules.module = 'subscriptions' "
-		. "";
-	require_once($ciniki['config']['core']['modules_dir'] . '/core/private/dbHashQuery.php');
-	$rc = ciniki_core_dbHashQuery($ciniki, $strsql, 'businesses', 'module');
-	if( $rc['stat'] != 'ok' ) {
-		return $rc;
-	}
-	if( !isset($rc['module']) || !isset($rc['module']['ruleset']) ) {
-		return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'382', 'msg'=>'Access denied.'));
-	}
 
 	//
 	// Sysadmins are allowed full access
@@ -51,10 +44,10 @@ function ciniki_subscriptions_checkAccess($ciniki, $business_id, $method, $subsc
 	//
 	// Check to see if the ruleset is valid
 	//
-	if( !isset($rulesets[$rc['module']['ruleset']]) ) {
+	if( !isset($rulesets[$rc['ruleset']]) ) {
 		return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'383', 'msg'=>'Access denied.'));
 	}
-	$ruleset = $rc['module']['ruleset'];
+	$ruleset = $rc['ruleset'];
 
 	// 
 	// Get the rules for the specified method
