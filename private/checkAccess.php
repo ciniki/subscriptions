f5a792c5a1ecf89dff06cfebe6868eb66f133712
Andrew Rivett <veggiefrog@gmail.com>
2014-02-27 16:22:21 -0500

    Simplified access check for module

--- private/checkAccess.php
+++ private/checkAccess.php
@@ -33,12 +33,6 @@ function ciniki_subscriptions_checkAccess($ciniki, $business_id, $method, $subsc
 	$modules = $rc['modules'];
 
 	//
-	// Load the rulesets for this module
-	//
-	ciniki_core_loadMethod($ciniki, 'ciniki', 'subscriptions', 'private', 'getRulesets');
-	$rulesets = ciniki_subscriptions_getRuleSets($ciniki);
-
-	//
 	// Sysadmins are allowed full access
 	//
 	if( ($ciniki['session']['user']['perms'] & 0x01) == 0x01 ) {
@@ -46,80 +40,26 @@ function ciniki_subscriptions_checkAccess($ciniki, $business_id, $method, $subsc
 	}
 
 	//
-	// Check to see if the ruleset is valid
+	// Users who are an owner or employee of a business can see the business atdo
 	//
-	if( !isset($rulesets[$rc['ruleset']]) ) {
-		return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'383', 'msg'=>'Access denied.'));
+	$strsql = "SELECT business_id, user_id FROM ciniki_business_users "
+		. "WHERE business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
+		. "AND user_id = '" . ciniki_core_dbQuote($ciniki, $ciniki['session']['user']['id']) . "' "
+		. "AND package = 'ciniki' "
+		. "AND status = 10 "
+		. "AND (permission_group = 'owners' OR permission_group = 'employees') "
+		. "";
+	ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbHashQuery');
+	$rc = ciniki_core_dbHashQuery($ciniki, $strsql, 'ciniki.businesses', 'user');
+	if( $rc['stat'] != 'ok' ) {
+		return $rc;
 	}
-	$ruleset = $rc['ruleset'];
-
-	// 
-	// Get the rules for the specified method
 	//
-	$rules = array();
-	if( isset($rulesets[$ruleset]['methods']) && isset($rulesets[$ruleset]['methods'][$method]) ) {
-		$rules = $rulesets[$ruleset]['methods'][$method];
-	} elseif( isset($rulesets[$ruleset]['default']) ) {
-		$rules = $rulesets[$ruleset]['default'];
-	} else {
-		return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'384', 'msg'=>'Access denied.'));
-	}
-
-
+	// If the user has permission, return ok
 	//
-	// Check the subscription_id is attached to the business
-	//
-	if( $subscription_id > 0 ) {
-		$strsql = "SELECT id, business_id FROM ciniki_subscriptions "
-			. "WHERE business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
-			. "AND id = '" . ciniki_core_dbQuote($ciniki, $subscription_id) . "' "
-			. "";
-		$rc = ciniki_core_dbHashQuery($ciniki, $strsql, 'ciniki.subscriptions', 'subscription');
-		if( $rc['stat'] != 'ok' ) {
-			return $rc;
-		}
-		//
-		// If nothing was returned, deny
-		// if business_id is not the same, deny (extra check)
-		// if subscription id is not the same, deny (extra check)
-		//
-		if( !isset($rc['subscription']) 
-			|| $rc['subscription']['business_id'] != $business_id 
-			|| $rc['subscription']['id'] != $subscription_id ) {
-			// Access denied!
-			return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'388', 'msg'=>'Access denied'));
-		}
-	}
-
-	//
-	// Apply the rules.  Any matching rule will allow access.
-	//
-
-	//
-	// If business_group specified, check the session user in the business_users table.
-	//
-	if( isset($rules['permission_groups']) && $rules['permission_groups'] > 0 ) {
-		//
-		// If the user is attached to the business AND in the one of the accepted permissions group, they will be granted access
-		//
-		$strsql = "SELECT business_id, user_id FROM ciniki_business_users "
-			. "WHERE business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
-			. "AND user_id = '" . ciniki_core_dbQuote($ciniki, $ciniki['session']['user']['id']) . "' "
-			. "AND status = 10 "
-			. "AND CONCAT_WS('.', package, permission_group) IN ('" . implode("','", $rules['permission_groups']) . "') "
-			. "";
-		$rc = ciniki_core_dbHashQuery($ciniki, $strsql, 'ciniki.businesses', 'user');
-		if( $rc['stat'] != 'ok' ) {
-			return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'385', 'msg'=>'Access denied.', 'err'=>$rc['err']));
-		}
-		
-		//
-		// If the user has permission, return ok
-		//
-		if( isset($rc['rows']) && isset($rc['rows'][0]) 
-			&& $rc['rows'][0]['user_id'] > 0 && $rc['rows'][0]['user_id'] == $ciniki['session']['user']['id'] ) {
-			return array('stat'=>'ok', 'modules'=>$rc['modules']);
-		}
+	if( isset($rc['rows']) && isset($rc['rows'][0]) 
+		&& $rc['rows'][0]['user_id'] > 0 && $rc['rows'][0]['user_id'] == $ciniki['session']['user']['id'] ) {
+		return array('stat'=>'ok', 'modules'=>$modules);
 	}
 
 	//
