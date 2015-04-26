d5b6525ddba182ca9217bcbcb730ba477fb26aa7
Andrew Rivett <veggiefrog@gmail.com>
2015-04-25 20:15:13 -0400

    Fixed emailList to only provide unique emails, moved to hooks

--- hooks/emailList.php
+++ hooks/emailList.php
@@ -10,11 +10,15 @@
 // Returns
 // -------
 //
-function ciniki_subscriptions_emailList($ciniki, $business_id, $subscription_ids) {
+function ciniki_subscriptions_hooks_emailList($ciniki, $business_id, $args) {
+
+	if( !isset($args['subscription_ids']) ) {
+		return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'2357', 'msg'=>'No subscriptions specified'));
+	}
 
 	ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbQuote');
 	ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbQuoteIDs');
-	ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbHashQuery');
+	ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbHashQueryIDTree');
 
 	//
 	// Pull the list of email addresses which we are allowed to send emails to.
@@ -24,7 +28,7 @@ function ciniki_subscriptions_emailList($ciniki, $business_id, $subscription_ids
 		. "ciniki_customer_emails.email, ciniki_subscriptions.uuid AS subscription_uuid "
 		. "FROM ciniki_subscription_customers, ciniki_subscriptions, ciniki_customers, ciniki_customer_emails "
 		. "WHERE ciniki_subscription_customers.business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
-		. "AND ciniki_subscription_customers.subscription_id IN (" . ciniki_core_dbQuoteIDs($ciniki, $subscription_ids) . ") "
+		. "AND ciniki_subscription_customers.subscription_id IN (" . ciniki_core_dbQuoteIDs($ciniki, $args['subscription_ids']) . ") "
 		. "AND ciniki_subscription_customers.status = 10 "
 		. "AND ciniki_subscription_customers.subscription_id = ciniki_subscriptions.id "
 		. "AND ciniki_subscriptions.business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
@@ -33,14 +37,19 @@ function ciniki_subscriptions_emailList($ciniki, $business_id, $subscription_ids
 		. "AND ciniki_customers.id = ciniki_customer_emails.customer_id "
 		. "AND ciniki_customer_emails.business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
 		. "AND (ciniki_customer_emails.flags&0x30) = 0 "	// Only emals that are ok to send to
+		. "ORDER BY ciniki_customer_emails.email "
 		. "";
-	$rc = ciniki_core_dbHashQuery($ciniki, $strsql, 'ciniki.subscriptions', 'email');
+	// Switched to use IDTree so we get unique email addresses only, no duplicates.
+	$rc = ciniki_core_dbHashQueryIDTree($ciniki, $strsql, 'ciniki.subscriptions', array(
+		array('container'=>'emails', 'fname'=>'email', 'fields'=>array('customer_id', 'customer_name', 'email', 'subscription_uuid')),
+		));
+//	$rc = ciniki_core_dbHashQuery($ciniki, $strsql, 'ciniki.subscriptions', 'email');
 	if( $rc['stat'] != 'ok' ) {
 		return $rc;
 	}
-	if( !isset($rc['rows']) ) {
+	if( !isset($rc['emails']) ) {
 		return array('stat'=>'ok', 'emails'=>array());
 	}
-	return array('stat'=>'ok', 'emails'=>$rc['rows']);	
+	return array('stat'=>'ok', 'emails'=>$rc['emails']);	
 }
 ?>
