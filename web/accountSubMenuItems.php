4c5f754304e038ed86f7494a6cff7991db570699
Andrew Rivett <veggiefrog@gmail.com>
2015-12-28 02:40:50 -0500

    Updated accoutn sub menu items

--- web/accountSubMenuItems.php
+++ web/accountSubMenuItems.php
@@ -19,6 +19,7 @@ function ciniki_subscriptions_web_accountSubMenuItems($ciniki, $settings, $busin
 
     $submenu[] = array('name'=>'Mailing Lists', 'priority'=>400, 
         'package'=>'ciniki', 'module'=>'subscriptions', 
+        'selected'=>($ciniki['request']['page'] == 'account' && isset($ciniki['request']['uri_split'][0]) && $ciniki['request']['uri_split'][0] == 'subscriptions')?'yes':'no',
         'url'=>$ciniki['request']['base_url'] . '/account/subscriptions');
 
 	return array('stat'=>'ok', 'submenu'=>$submenu);
