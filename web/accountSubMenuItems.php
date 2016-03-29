a6ba705fbd7481d2d93bb1a56badd6bc3f9df73b
Andrew Rivett <veggiefrog@gmail.com>
2016-03-28 20:51:53 -0400

    Added check for active subscriptions before showing subMenuItem

--- web/accountSubMenuItems.php
+++ web/accountSubMenuItems.php
@@ -16,11 +16,20 @@ function ciniki_subscriptions_web_accountSubMenuItems($ciniki, $settings, $busin
     //
     // FIXME: Check if any public subscriptions available for company
     //
+    $strsql = "SELECT COUNT(*) AS num "
+        . "FROM ciniki_subscriptions "
+        . "WHERE business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
+        . "AND status = 10 "
+        . "AND (flags&0x01) = 0x01 "
+        . "";
+    $rc = ciniki_core_dbHashQuery($ciniki, $strsql, 'ciniki.subscriptions', 'total');
+    if( $rc['stat'] == 'ok' && isset($rc['total']['num']) && $rc['total']['num'] > 0 ) {
+        $submenu[] = array('name'=>'Mailing Lists', 'priority'=>400, 
+            'package'=>'ciniki', 'module'=>'subscriptions', 
+            'selected'=>($ciniki['request']['page'] == 'account' && isset($ciniki['request']['uri_split'][0]) && $ciniki['request']['uri_split'][0] == 'subscriptions')?'yes':'no',
+            'url'=>$ciniki['request']['base_url'] . '/account/subscriptions');
+    }
 
-    $submenu[] = array('name'=>'Mailing Lists', 'priority'=>400, 
-        'package'=>'ciniki', 'module'=>'subscriptions', 
-        'selected'=>($ciniki['request']['page'] == 'account' && isset($ciniki['request']['uri_split'][0]) && $ciniki['request']['uri_split'][0] == 'subscriptions')?'yes':'no',
-        'url'=>$ciniki['request']['base_url'] . '/account/subscriptions');
 
 	return array('stat'=>'ok', 'submenu'=>$submenu);
 }
