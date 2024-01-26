070044ca9c297e20b3778c9223751113abe6da04
Andrew Rivett <veggiefrog@gmail.com>
2024-01-25 20:53:33 -0500

    Added customerMerge

---
+++ hooks/customerMerge.php
@@ -0,0 +1,60 @@
+<?php
+//
+// Description
+// -----------
+// Merge the subscriptions from secondary_customer_id into primary_customer_id
+//
+// Arguments
+// ---------
+//
+// Returns
+// -------
+//
+function ciniki_subscriptions_hooks_customerMerge($ciniki, $tnid, $args) {
+
+    ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'objectUpdate');
+    ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbHashQuery');
+
+    if( !isset($args['primary_customer_id']) || $args['primary_customer_id'] == '' 
+        || !isset($args['secondary_customer_id']) || $args['secondary_customer_id'] == '' ) {
+        return array('stat'=>'ok');
+    }
+
+    //
+    // Keep track of how many items we've updated
+    //
+    $updated = 0;
+
+    //
+    // Get the list of message to update
+    //
+    $strsql = "SELECT id "
+        . "FROM ciniki_subscription_customers "
+        . "WHERE tnid = '" . ciniki_core_dbQuote($ciniki, $tnid) . "' "
+        . "AND customer_id = '" . ciniki_core_dbQuote($ciniki, $args['secondary_customer_id']) . "' "
+        . "";
+    $rc = ciniki_core_dbHashQuery($ciniki, $strsql, 'ciniki.subscriptions', 'items');
+    if( $rc['stat'] != 'ok' ) {
+        return array('stat'=>'fail', 'err'=>array('code'=>'ciniki.subscriptions.5', 'msg'=>'Unable to find subscription customers', 'err'=>$rc['err']));
+    }
+    $items = $rc['rows'];
+    foreach($items as $i => $row) {
+        $rc = ciniki_core_objectUpdate($ciniki, $tnid, 'ciniki.subscriptions.custaomer', $row['id'], array('customer_id'=>$args['primary_customer_id']), 0x04);
+        if( $rc['stat'] != 'ok' ) {
+            return array('stat'=>'fail', 'err'=>array('code'=>'ciniki.subscriptions.14', 'msg'=>'Unable to update subscription customer.', 'err'=>$rc['err']));
+        }
+        $updated++;
+    }
+
+    if( $updated > 0 ) {
+        //
+        // Update the last_change date in the tenant modules
+        // Ignore the result, as we don't want to stop user updates if this fails.
+        //
+        ciniki_core_loadMethod($ciniki, 'ciniki', 'tenants', 'private', 'updateModuleChangeDate');
+        ciniki_tenants_updateModuleChangeDate($ciniki, $tnid, 'ciniki', 'subscriptions');
+    }
+
+    return array('stat'=>'ok', 'updated'=>$updated);
+}
+?>
